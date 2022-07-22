8f364d24972bbea1dc115547051d5815f6302834
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-07-22 15:29:13 +0500

    Create elements in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Header(props) {
   return (
-    <header>
-      header
+    <header className="header">
+      <h1 className="header__heading">Shopping Cart</h1>
+      <ul className="header__links">
+        <li><Link className="link" to="/">Home</Link></li>
+        <li><Link className="link" to="/shop">Shop</Link></li>
+      </ul>
     </header>
   )
 }
