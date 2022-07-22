0397538f739fbe0efc3f6c121f4d230e8aafbd2b
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-07-22 15:30:18 +0500

    Create elements in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function Home(props) {
   return (
-    <section>
-      home
+    <section className="content home">
+      <img className="home__hero" src="" alt="hero" />
+      <p className="home__main-text">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident temporibus et quis veniam, assumenda odio rerum placeat, voluptas fugiat dicta tenetur quos asperiores quibusdam non voluptatum architecto quia ducimus recusandae.
+      </p>
+      <p className="home__call-to-action">
+        What are you waiting for? <Link to="/shop">Go to the shop NOW!</Link>
+      </p>
     </section>
   )
 }
