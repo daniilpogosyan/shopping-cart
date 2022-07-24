2fe0d5b102496fb0e4a221a68c08feaebaa3787b
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-07-24 18:00:48 +0500

    Stick footer to the bottom

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -2,13 +2,16 @@ import { Outlet } from 'react-router-dom';
 
 import Header from './Header';
 import Footer from './Footer';
+import './style/PageLayout.css';
 
 export default function PageLayout(props) {
   return (
-    <>
+    <div className='page-layout'>
       <Header />
-      <Outlet />
+      <div className="main">
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </div>
   )
 }
